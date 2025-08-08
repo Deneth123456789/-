ab5eb64617fdff7730c5ab4410aeb636fb94281a
Deneth123456789 <Deneth333333@gmail.com>
2025-08-08 17:56:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "oUx21LTB#OkGgnKBwf6jnFS2h1plolYTB5T6vfxbThybU2FAuPz0",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DENETH-YT Is Alive Now😍*",
 BOT_OWNER: '94768805840',  // Replace with the owner's phone number
 
